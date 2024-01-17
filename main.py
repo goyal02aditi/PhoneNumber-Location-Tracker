a8408f38d7db46c4f83fc14984291586953886dc
Aditi Goyal <145578961+goyal02aditi@users.noreply.github.com>
2024-01-17 23:27:22 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import phonenumbers
+from test import number
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from phonenumbers import geocoder
+ch_number = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_number, "en"))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from phonenumbers import carrier
+service_number = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "en"))
